4a3faf17e7ae8d2a120679e56f8c881f3c352c39
alexaaaant <alexaaaant@gmail.com>
2019-09-06 21:18:14 +0300

    Реализован drag and drop

--- src/App.tsx
+++ src/App.tsx
@@ -45,6 +45,40 @@ class App extends React.Component<{}, IStateForApp> {
     }
   }
 
+  handleDragStart = (event: React.DragEvent<HTMLElement>) => {
+    event.dataTransfer.effectAllowed = 'move'
+    if (event.currentTarget.dataset.id) {
+      event.dataTransfer.setData('text', event.currentTarget.dataset.id);
+    }
+  }
+
+  handleDrop = (event: React.DragEvent<HTMLElement>) => {
+    const elemId = Number(event.dataTransfer.getData('text'));
+    const sourceElemId = Number(event.currentTarget.dataset.id);
+    const { data } = this.state;
+    let copyData = new Map(data);
+    let sourceElem = copyData.get(sourceElemId);
+    let elem = copyData.get(elemId);
+    if (elem && sourceElem) {
+      let parentElem = elem.parentId !== null && copyData.get(elem.parentId);
+      if (parentElem) {
+        parentElem.children = parentElem.children.filter((child) => child.id !== elem!.id);
+      }
+      elem.parentId = sourceElem.id;
+      sourceElem.children.push(elem);
+    }
+    this.setState({
+      data: copyData,
+    })
+  }
+
+  handleDragOver = (event: React.DragEvent<HTMLElement>) => {
+    event.preventDefault();
+    if (event.currentTarget.dataset.type !== FOLDER) {
+      event.dataTransfer.dropEffect = 'none';
+    }
+  }
+
   render() {
     const { data, currentFolderId } = this.state;
     const currentFolder: IData | undefined = data.get(currentFolderId);
@@ -53,12 +87,21 @@ class App extends React.Component<{}, IStateForApp> {
         {currentFolder ?
           <>
             <div className="top_panel">
-              <div className="top_panel-up" onClick={this.handleClickToUp}>Up</div>
-              <div className="top_panel-name">{currentFolder.name}</div>
+              <div className="top_panel-up" onClick={this.handleClickToUp}>Come back</div>
+              <div className="top_panel-name">{`Current position: ${currentFolder.name}`}</div>
             </div>
             <div className="cells_container">
               {currentFolder.children && currentFolder.children.map((child) => (
-                <div className="cells_container-cell" key={child.id} data-id={child.id} data-type={child.type} onDoubleClick={this.handleDoubleClick}>
+                <div
+                  className="cells_container-cell"
+                  onDragStart={this.handleDragStart}
+                  onDrop={this.handleDrop}
+                  onDragOver={this.handleDragOver}
+                  draggable
+                  key={child.id}
+                  data-id={child.id}
+                  data-type={child.type}
+                  onDoubleClick={this.handleDoubleClick}>
                   <img className="cells_container-cell-img" src={child.type === FOLDER ? folderUrl : fileUrl} alt='folder..'></img>
                   <span className="cells_container-cell-name">{child.name}</span>
                 </div>
