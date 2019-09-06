c8578a33fd82b81a9bd87da0925bb0aef9c29d3b
alexaaaant <alexaaaant@gmail.com>
2019-09-06 18:27:47 +0300

    Реализован стейт

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-    </div>
-  );
+interface IData {
+  id: number,
+  name: string,
+  type: string,
+  children: IData,
+}
+
+interface IStateForApp {
+  currentFolderId: number,
+  data: Map<number, IData>,
+}
+
+class App extends React.Component<{}, IStateForApp> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      currentFolderId: 0,
+      data: new Map(),
+    }
+  }
+  render() {
+    return (
+      <div></div>
+    )
+  }
 }
 
 export default App;
