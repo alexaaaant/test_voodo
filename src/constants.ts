861c18fd6aa1d1e18f98271a83b67ca072d31e28
alexaaaant <alexaaaant@gmail.com>
2019-09-06 20:00:00 +0300

    Добавлены константы

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const folderUrl = 'https://api.icons8.com/download/5d0df72fd1e3f7209d05c11feab5d97faa2730b7/Color/PNG/512/Very_Basic/opened_folder-512.png';
+export const fileUrl = 'http://s1.iconbird.com/ico/2013/12/505/w450h4001385925290Document.png';
+
+export const FOLDER = 'folder';
