ef1977a744430ab0b31d9b9098511eed634aec77
alexaaaant <alexaaaant@gmail.com>
2019-09-07 19:52:16 +0300

    Исправлены отступы

--- src/components/ContextMenu.tsx
+++ src/components/ContextMenu.tsx
@@ -3,90 +3,90 @@ import './ContextMenu.css';
 import { FOLDER, FILE } from '../constants';
 
 interface IPropsForContextMenu {
-    closeContextMenu(): void,
-    changeElement(id: number | null): void,
-    createElement(type: string): void,
-    coords: { x: number, y: number },
-    type: string,
-    elementId: number | null,
+  closeContextMenu(): void,
+  changeElement(id: number | null): void,
+  createElement(type: string): void,
+  coords: { x: number, y: number },
+  type: string,
+  elementId: number | null,
 }
 
 interface IStateForContextMenu {
-    coords: IPropsForContextMenu['coords'],
-    type: IPropsForContextMenu['type'],
-    selectedElementId: IPropsForContextMenu['elementId'],
+  coords: IPropsForContextMenu['coords'],
+  type: IPropsForContextMenu['type'],
+  selectedElementId: IPropsForContextMenu['elementId'],
 }
 
 class ContextMenu extends React.Component<IPropsForContextMenu, IStateForContextMenu> {
-    private contextMenu: React.RefObject<HTMLDivElement>;
+  private contextMenu: React.RefObject<HTMLDivElement>;
 
-    constructor(props: IPropsForContextMenu) {
-        super(props);
-        this.contextMenu = React.createRef();
-        this.state = {
-            coords: {
-                x: 0,
-                y: 0,
-            },
-            type: '',
-            selectedElementId: null,
-        }
+  constructor(props: IPropsForContextMenu) {
+    super(props);
+    this.contextMenu = React.createRef();
+    this.state = {
+      coords: {
+        x: 0,
+        y: 0,
+      },
+      type: '',
+      selectedElementId: null,
     }
+  }
 
-    componentDidMount() {
-        const { coords, type, elementId } = this.props;
-        document.addEventListener('mousedown', (e) => this.handleClickOutside(e));
+  componentDidMount() {
+    const { coords, type, elementId } = this.props;
+    document.addEventListener('mousedown', (e) => this.handleClickOutside(e));
+    this.setState({
+      coords,
+      type,
+      selectedElementId: elementId,
+    })
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', (e) => this.handleClickOutside(e));
+  }
+
+  handleClickOutside = (event: any) => {
+    if (this.contextMenu.current && !this.contextMenu.current.contains(event.target)) {
+      event.button !== 2 && this.props.closeContextMenu();
+      if (event.button === 2) {
+        let x = event.clientX;
+        let y = event.clientY;
+        let type = event.target.dataset.type;
+        let id = Number(event.target.dataset.id);
         this.setState({
-            coords,
-            type,
-            selectedElementId: elementId,
+          coords: { x, y },
+          type,
+          selectedElementId: id,
         })
+      }
     }
+  }
 
-    componentWillUnmount() {
-        document.removeEventListener('mousedown', (e) => this.handleClickOutside(e));
+  renderContextMenu = () => {
+    const { type, coords, selectedElementId } = this.state;
+    const { changeElement, createElement } = this.props;
+    switch (type) {
+      case FOLDER:
+      case FILE: {
+        return <div style={{ left: coords.x, top: coords.y }} ref={this.contextMenu} className='context_menu'>
+          <span className='context_menu-item' onClick={(e) => changeElement(selectedElementId)}>Rename element</span>
+        </div>
+      }
+      default:
+        return <div style={{ left: coords.x, top: coords.y }} className='context_menu' ref={this.contextMenu}>
+          <span className='context_menu-item' onClick={(e) => createElement(FOLDER)}>Create folder</span>
+          <span className='context_menu-item' onClick={(e) => createElement(FILE)}>Create file</span>
+        </div>
     }
+  }
 
-    handleClickOutside = (event: any) => {
-        if (this.contextMenu.current && !this.contextMenu.current.contains(event.target)) {
-            event.button !== 2 && this.props.closeContextMenu();
-            if (event.button === 2) {
-                let x = event.clientX;
-                let y = event.clientY;
-                let type = event.target.dataset.type;
-                let id = Number(event.target.dataset.id);
-                this.setState({
-                    coords: { x, y },
-                    type,
-                    selectedElementId: id,
-                })
-            }
-        }
-    }
-
-    renderContextMenu = () => {
-        const { type, coords, selectedElementId } = this.state;
-        const { changeElement, createElement } = this.props;
-        switch (type) {
-            case FOLDER:
-            case FILE: {
-                return <div style={{ left: coords.x, top: coords.y }} ref={this.contextMenu} className='context_menu'>
-                    <span className='context_menu-item' onClick={(e) => changeElement(selectedElementId)}>Rename element</span>
-                </div>
-            }
-            default:
-                return <div style={{ left: coords.x, top: coords.y }} className='context_menu' ref={this.contextMenu}>
-                    <span className='context_menu-item' onClick={(e) => createElement(FOLDER)}>Create folder</span>
-                    <span className='context_menu-item' onClick={(e) => createElement(FILE)}>Create file</span>
-                </div>
-        }
-    }
-
-    render() {
-        return (
-            this.renderContextMenu()
-        )
-    }
+  render() {
+    return (
+      this.renderContextMenu()
+    )
+  }
 };
 
 export default ContextMenu;
